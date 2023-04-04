d2ed27dc91e78f7fd1873f888f33b207a12db722
Dmytro Vasyliev <vasylievdmytro04@gmail.com>
2023-04-04 02:39:07 -0700

    init commit

---
+++ reddittelegrambot/logs/logger.py
@@ -0,0 +1,29 @@
+
+import logging
+from logging.handlers import RotatingFileHandler
+import os
+
+
+file_log_format = logging.Formatter(
+    "%(asctime)s %(levelname)s %(funcName)s(%(lineno)d) %(message)s")
+stream_log_format = logging.Formatter("%(asctime)s %(message)s", "%H:%M:%S")
+logFile = "info.log"
+
+file_handler = RotatingFileHandler(
+    logFile, mode="a", maxBytes=15 * 1024 * 1024, backupCount=2, encoding=None, delay=0)
+file_handler.setFormatter(file_log_format)
+
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(stream_log_format)
+
+log = logging.getLogger(__name__)
+
+if "DEBUG" in os.environ:
+    log.setLevel(logging.DEBUG)
+    print("debug logging")
+else:
+    log.setLevel(logging.INFO)
+    print("info loggin")
+
+log.addHandler(stream_handler)
+log.addHandler(file_handler)
